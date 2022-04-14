aa3ea4a3d467383fa232951c66e4f8146bd5d5ca
Ivan <ivanma819@gmail.com>
2022-04-13 22:14:16 -0700

    Add change

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -15,16 +15,17 @@ using std::chrono::steady_clock;
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   int BLOCK_SIZE = 64;
+  int inner_BLOCK_SIZE = 1024;
   // matrix multiplication
 #pragma omp parallel num_threads(8)
   {
 #pragma omp for
     for (int i = 0; i < kI; i += BLOCK_SIZE) {
       for (int k = 0; k < kK; k += BLOCK_SIZE) {
-        for (int j = 0; j < kJ; j += BLOCK_SIZE) {
+        for (int j = 0; j < kJ; j += inner_BLOCK_SIZE) {
           int i2 = i + BLOCK_SIZE;
           int j2 = j + BLOCK_SIZE;
-          int k2 = k + BLOCK_SIZE;
+          int k2 = k + inner_BLOCK_SIZE;
           for (int ii = i; ii < i2; ii += 4) {
             for (int kk = k; kk < k2; kk += 4) {
               for (int jj = j; jj < j2; ++jj) {
