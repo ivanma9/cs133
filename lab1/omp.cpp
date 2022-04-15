5fb296cb3247e43a074ff5cfd27f354d5bf4faae
Ivan <ivanma819@gmail.com>
2022-04-14 17:13:24 -0700

    Add change

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -21,8 +21,8 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
 {
 #pragma omp parallel for
   for (int i = 0; i < kI; ++i){
-    for (int j = 0; j < kJ; ++j){
-      for (int k = 0; k < kK; ++k){
+    for (int k = 0; k < kK; ++k){
+      for (int j = 0; j < kJ; ++j){
         c[i][j] += a[i][k] * b[k][j];        
       }
     }
