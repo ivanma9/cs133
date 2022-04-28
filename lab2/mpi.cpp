857de63c0445ff0ecfd7780fe38f70df57dc98f9
Ivan <ivanma819@gmail.com>
2022-04-27 20:44:28 -0700

    Add change

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -25,43 +25,44 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ], float c[k
     float *bBuffer = (float *)lab2::aligned_alloc(1024, bSize * sizeof(float));
     float *cBuffer = (float *)lab2::aligned_alloc(1024, cSize * sizeof(float));
 
+        memset(cBuffer, 0, sizeof(float) * cSize);
 
 
     int rowSize = kI / numProcesses;
 
     /* ------------------------ 1 Using Scatter and Bcast ----------------------- */
-    if (rank == 0) {
-        memcpy(bBuffer, b, sizeof(float) * bSize);
-        memset(cBuffer, 0, sizeof(float) * cSize);
-    }
-
-    MPI_Scatter(a, aSize, MPI_FLOAT, aBuffer, aSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(bBuffer, bSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    /* --------------------- 2 Using Isend, Irecv, and Wait --------------------- */
-    // int offset = rowSize;
-    // MPI_Status status;
-    // MPI_Request request;
-    // if (rank == 0) {
-    //     memcpy(aBuffer, a, sizeof(float) * aSize);
-    //     for (int i = 1; i < numProcesses; i++) {
-    //         MPI_Isend(&a[offset][0], aSize, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &request);
-    //         offset += rowSize;
-    //     }
-    // } else {
-    //     MPI_Irecv(aBuffer, aSize, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &request);
-    //     MPI_Wait(&request, &status);
-    // }
-
     // if (rank == 0) {
     //     memcpy(bBuffer, b, sizeof(float) * bSize);
-    //     for (int i = 1; i < numProcesses; i++) {
-    //         MPI_Isend(b, bSize, MPI_FLOAT, i, 2, MPI_COMM_WORLD, &request);
-    //     }
-    // } else {
-    //     MPI_Irecv(bBuffer, bSize, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &request);
-    //     MPI_Wait(&request, &status);
+    //     memset(cBuffer, 0, sizeof(float) * cSize);
     // }
+
+    // MPI_Scatter(a, aSize, MPI_FLOAT, aBuffer, aSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    // MPI_Bcast(bBuffer, bSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    /* --------------------- 2 Using Isend, Irecv, and Wait --------------------- */
+    int offset = rowSize;
+    MPI_Status status;
+    MPI_Request request;
+    if (rank == 0) {
+        memcpy(aBuffer, a, sizeof(float) * aSize);
+        for (int i = 1; i < numProcesses; i++) {
+            MPI_Isend(&a[offset][0], aSize, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &request);
+            offset += rowSize;
+        }
+    } else {
+        MPI_Irecv(aBuffer, aSize, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &status);
+    }
+
+    if (rank == 0) {
+        memcpy(bBuffer, b, sizeof(float) * bSize);
+        for (int i = 1; i < numProcesses; i++) {
+            MPI_Isend(b, bSize, MPI_FLOAT, i, 2, MPI_COMM_WORLD, &request);
+        }
+    } else {
+        MPI_Irecv(bBuffer, bSize, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &status);
+    }
 
     /* --------------------------- X 3 Using Send, Recv --------------------------- */
     // int offset = rowSize;
@@ -120,20 +121,20 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ], float c[k
     }
 
     /* ----------------------------- 1 Using gather ----------------------------- */
-    MPI_Gather(cBuffer, cSize, MPI_FLOAT, c, cSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    // MPI_Gather(cBuffer, cSize, MPI_FLOAT, c, cSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     /* ----------------------- 2 Using Isend, Irecv, Wait ----------------------- */
-    // if (rank == 0) {
-    //     offset = rowSize;
-    //     memcpy(c, cBuffer, sizeof(float) * cSize);
-    //     for (int i = 1; i < numProcesses; i++) {
-    //         MPI_Irecv(&c[offset][0], cSize, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &request);
-    //         offset += rowSize;
-    //         MPI_Wait(&request, &status);
-    //     }
-    // } else {
-    //     MPI_Isend(cBuffer, cSize, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &request);
-    // }
+    if (rank == 0) {
+        offset = rowSize;
+        memcpy(c, cBuffer, sizeof(float) * cSize);
+        for (int i = 1; i < numProcesses; i++) {
+            MPI_Irecv(&c[offset][0], cSize, MPI_FLOAT, i, 1, MPI_COMM_WORLD, &request);
+            offset += rowSize;
+            MPI_Wait(&request, &status);
+        }
+    } else {
+        MPI_Isend(cBuffer, cSize, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &request);
+    }
 
     /* --------------------------- X 3 Using Send, Recv --------------------------- */
     // if (rank == 0) {
