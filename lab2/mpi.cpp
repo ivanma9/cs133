5dc0a19648d64d1761d3d50e3ae7116447b18f61
Ivan <ivanma819@gmail.com>
2022-04-27 21:49:56 -0700

    Add change

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -20,49 +20,45 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ], float c[k
     int aSize = kI * kK / numProcesses;
     int bSize = kK * kJ;
     int cSize = kI * kJ / numProcesses;
+    int OUTER_BLOCK_SIZE = 32;
+    int INNER_BLOCK_SIZE = 128;
+    int BLOCK_SIZE = 8;
 
     float *aligned_a = (float *)lab2::aligned_alloc(1024, aSize * sizeof(float));
     float *aligned_b = (float *)lab2::aligned_alloc(1024, bSize * sizeof(float));
     float *aligned_c = (float *)lab2::aligned_alloc(1024, cSize * sizeof(float));
 
-        memset(aligned_c, 0, sizeof(float) * cSize);
-
-
 
     MPI_Status status;
     if (rank == 0) {
+        memset(aligned_c, 0, sizeof(float) * cSize);
         memcpy(aligned_b, b, sizeof(float) * bSize);
         for (int i = 1; i < numProcesses; i++) {
             MPI_Send(b, bSize, MPI_FLOAT, i, 2, MPI_COMM_WORLD);
         }
-    } else {
+    } 
+    else {
         MPI_Recv(aligned_b, bSize, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &status);
     }
     MPI_Scatter(a, aSize, MPI_FLOAT, aligned_a, aSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    int iBlockSize = 32;
-    int jBlockSize = 128;
-    int kBlockSize = 8;
 
-    float temp_buffer;
     int rowSize = kI / numProcesses;
 
-    for (int i = 0; i < rowSize; i += iBlockSize) {
-        for (int k = 0; k < kK; k += kBlockSize) {
-            for (int j = 0; j < kJ; j += jBlockSize) {
-                int bi = i + iBlockSize;
-                int bj = j + jBlockSize;
-                int bk = k + kBlockSize;
+    for (int i = 0; i < rowSize; i += OUTER_BLOCK_SIZE) {
+        for (int k = 0; k < kK; k += BLOCK_SIZE) {
+            for (int j = 0; j < kJ; j += INNER_BLOCK_SIZE) {
+                int bi = i + OUTER_BLOCK_SIZE;
+                int bj = j + INNER_BLOCK_SIZE;
+                int bk = k + BLOCK_SIZE;
 
                 for (int ii = i; ii < bi; ii++) {
                     for (int jj = j; jj < bj; jj++) {
-                        temp_buffer = 0;
-
+                        float temp_register = 0;
                         for (int kk = k; kk < bk; kk++) {
-                            temp_buffer += aligned_a[ii * kJ + kk] * aligned_b[kk * kJ + jj];
+                            temp_register += aligned_a[ii * kJ + kk] * aligned_b[kk * kJ + jj];
                         }
-
-                        aligned_c[ii * kJ + jj] += temp_buffer;
+                        aligned_c[ii * kJ + jj] += temp_register;
                     }
                 }
             }
